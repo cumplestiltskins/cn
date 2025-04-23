6ed1b3f83e12f89585320426696ea9c9ab74ef41
cumplestiltskins <muditmeshram47@gmail.com>
2025-04-23 07:29:59 +0530

    Create 3node.tcl

---
+++ 3node.tcl
@@ -0,0 +1,37 @@
+set ns [new Simulator]
+set nf [open out.nam w]
+$ns namtrace-all $nf
+
+set n0 [$ns node]
+set n1 [$ns node]
+set n2 [$ns node]
+
+$ns duplex-link $n0 $n1 1Mb 10ms DropTail
+$ns duplex-link $n1 $n2 1Mb 10ms DropTail
+
+$ns duplex-link-op $n0 $n1 orient right
+$ns duplex-link-op $n1 $n2 orient right
+
+set udp [new Agent/UDP]
+$ns attach-agent $n0 $udp
+
+set null [new Agent/Null]
+$ns attach-agent $n2 $null
+
+$ns connect $udp $null
+
+set cbr [new Application/Traffic/CBR]
+$cbr set packetSize_ 500
+$cbr set interval_ 0.005
+$cbr attach-agent $udp
+
+$ns at 1.0 "$cbr start"
+$ns at 4.0 "finish"
+proc finish {} {
+    global ns nf
+    $ns flush-trace
+    close $nf
+    exec nam out.nam &
+    exit 0
+}
+$ns run
